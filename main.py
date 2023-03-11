0ade1e6e0e8e1b098f2657529de05e7db5695413
GuardCN <1534514157@qq.com>
2023-03-11 18:41:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+num_int = 123
+num_flo = 1.23
+
+num_new = num_int + num_flo
+
+print("datatype of num_int:",type(num_int))
+print("datatype of num_flo:",type(num_flo))
+
+print("Value of num_new:",num_new)
+print("datatype of num_new:",type(num_new))
+
+num_str = "456"
+x = int(num_str)
+
+print(x)
+print(x+num_int)
